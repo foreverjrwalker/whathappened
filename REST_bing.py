372a27636973577d42496d88082dd60e26082b6b
Michael <starwalkerx18@gmail.com>
2013-05-05 10:09:02 -0500

    fix

--- REST_bing.py
+++ REST_bing.py
@@ -5,5 +5,5 @@ import urllib
 BING_BASE = 'https://api.datamarket.azure.com/Bing/Search/v1/RelatedSearch?Query='
 
 def getRelatedSearchWords(query):
-    "Authorization: Basic " . base64_encode($accountKey . ":" . $accountKey
+    #"Authorization: Basic " . base64_encode($accountKey . ":" . $accountKey
     print urllib.urlencode(BING_BASE + query)
